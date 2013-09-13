18741d197137afa5ebd1be78f55acbd9122df1fc
Casey Li <casey.li@gmail.com>
2013-09-13 08:29:05 -0400

    Fixed location adding.

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -47,7 +47,7 @@ class LocationsController < ApplicationController
 
     respond_to do |format|
       if @location.save
-        format.html { redirect_to company_locations_path(@company), notice: 'Location was successfully created.' }
+        format.html { redirect_to @company, notice: 'Location was successfully created.' }
         format.json { render json: @location, status: :created, location: @location }
       else
         format.html { render action: "new" }
